0e575401044e230b1c2374eebc00825456d8f952
Kendall Montenegro Vega <kendallmontenegrovega@gmail.com>
2020-10-07 09:51:03 -0600

    Creación de la barra de navegación y la página principal del sitio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppHeaderComponent } from './app-header/app-header.component';
+import { AppNavbarComponent } from './app-navbar/app-navbar.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AppHeaderComponent, AppNavbarComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//LOCALIZATION l10N
+//ACCESIBILITY a10y
